105f690d6d95a11058dde7119f0b00c82b9c566a
ahmedahshlw <ahmedit5216@gmail.com>
2024-08-05 15:10:35 +0300

    solution commit

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,18 @@
+const WeatherForecast = ({
+  founder: { day, img, imgAlt, conditions, time },
+}) => {
+  return (
+    <div className="weather">
+      <h2>{day}</h2>
+      <img src={img} alt={imgAlt} />
+      <p>
+        <span>conditions: {conditions}</span>
+      </p>
+      <p>
+        <span>time: {time}</span>
+      </p>
+    </div>
+  );
+};
+
+export default WeatherForecast;
